02ac293a412f7e9d78884bcc2ac9939764eb7891
bsh6463 <bsh9510@gmail.com>
2022-03-15 22:04:14 +0900

    Read and Update data with Room

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -1,14 +1,14 @@
 package com.example.inventory
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.inventory.data.Item
 import com.example.inventory.data.ItemDao
 import kotlinx.coroutines.launch
 import java.lang.IllegalArgumentException
 
 class InventoryViewModel(private val itemDao: ItemDao) : ViewModel(){
+
+    val allItems: LiveData<List<Item>> = itemDao.getItems().asLiveData()
 
     private fun insertItem(item: Item){
         //ewModelScope는 ViewModel이 소멸될 때 하위 코루틴을 자동으로 취소하는 ViewModel 클래스의 확장 속성입니다.
@@ -35,6 +35,56 @@ class InventoryViewModel(private val itemDao: ItemDao) : ViewModel(){
             return false
         }
         return true
+    }
+
+    fun retrieveItem(id:Int) : LiveData<Item>{
+        return itemDao.getItem(id).asLiveData()
+    }
+
+    private fun updateItem(item:Item){
+        viewModelScope.launch {
+            itemDao.update(item)
+        }
+    }
+
+    fun sellItem(item:Item){
+        if(item.quantityInStock > 0){
+            val newItem = item.copy(quantityInStock = item.quantityInStock - 1)
+            updateItem(newItem)
+        }
+    }
+
+    fun isStockAvailable(item:Item):Boolean{
+        return (item.quantityInStock > 0)
+    }
+
+    fun deleteItem (item:Item){
+        viewModelScope.launch {
+            itemDao.delete(item)
+        }
+    }
+
+    private fun getUpdatedItemEntry(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String): Item{
+        return Item(
+            id = itemId,
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    fun updateItem(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ) {
+        val updatedItem = getUpdatedItemEntry(itemId, itemName, itemPrice, itemCount)
+        updateItem(updatedItem)
     }
 }
 
---
+++ app/src/main/java/com/example/inventory/ItemListAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.inventory
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.inventory.data.Item
+import com.example.inventory.databinding.ItemListItemBinding
+
+
+class ItemListAdapter(private val onItemClicked: (Item) -> Unit) : ListAdapter<Item, ItemListAdapter.ItemViewHolder>(DiffCallback){
+
+
+    //viewHolder 생성, view 초기화
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+
+        return ItemViewHolder(
+            ItemListItemBinding.inflate(
+                LayoutInflater.from(parent.context))
+        )
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.itemView.setOnClickListener {
+            onItemClicked(current)
+        }
+
+          }
+
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ItemViewHolder(private var binding: ItemListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Item){
+            binding.apply {
+                itemName.text = item.itemName
+                itemPrice.text = item.itemPrice.toString()
+                itemQuantity.text = item.quantityInStock.toString()
+            }
+        }
+    }
+
+    companion object{
+        val DiffCallback =  object: DiffUtil.ItemCallback<Item>() {
+            override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+                return oldItem.itemName == newItem.itemName
+            }
+
+        }
+    }
+}
